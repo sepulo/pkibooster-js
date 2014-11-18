23c5ec1994a5389fbd498225cea0df1458793421
Reza Gouki <reza.goki@gmail.com>
2014-11-18 17:55:46 +0330

    findByHash function was added to PKCS11Storage class.

--- js/pkibooster.js
+++ js/pkibooster.js
@@ -535,6 +535,27 @@ function PKCS11Storage () {
         else
             return this._innerStorage.Opened;
     }
+
+    /**
+     *  Returns the certificate object by certificate thumbprint(SHA-1 hash).
+     *
+     * @param thumbprint {String} Certificate SHA-1 hash(thumbprint)
+     * @return {_Certificate}
+     */
+    this.findByHash = function(thumbprint) {
+        var innerCert = null;
+        //todo: first lookup the cert in the internal map
+        if(applet)
+            innerCert = this._innerStorage.findByHash(thumbprint);
+        else
+            innerCert = this._innerStorage.FindByHash(thumbprint);
+
+        var cert = new _Certificate();
+        cert._innerCertificate = innerCert;
+        cert._loadInternals();
+
+        return cert;
+    }
 }
 
 /********************************* PKCS11SessionInfo *************************/
